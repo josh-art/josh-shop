249c9cde18b8af2b179f6688ec00be7498fde914
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-31 16:39:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 #build the project
 echo "building the project..."
-python3.9 pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "make migrations..."
 python3.9 manage.py makemigrations 
